0daab040d963f619c598e2a002948a8ab155efdd
AndrewXu23 <zxu232@gmail.com>
2022-11-01 04:45:26 -0700

    init commit

---
+++ routes/register.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+import { register } from "../controller/register.js";
+// import myDB from "../db/myDB.js";
+
+router.post("/", register);
+// router.post("/", (req, res) => {
+//     myDB.createUser(user);
+//   });
+router.get("/", (req, res) => {
+  res.status(200).redirect("..");
+});
+
+export default router;
